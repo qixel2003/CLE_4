52fec8a18c6c9a0df505a030562eb109bd18537d
eline <1096534@hr.nl>
2025-06-23 21:15:27 +0200

    added poolbg

--- src/js/poolgebied/pool.js
+++ src/js/poolgebied/pool.js
@@ -6,20 +6,27 @@ import { PoolBorderLeft } from "./poolBorderLeft.js";
 import { PoolBorderRight } from "./poolBorderRight.js";
 import { PoolBorderTop } from "./poolBorderTop.js";
 import { PoolBorderBottom } from "./poolBorderBottom.js";
+import { PoolBackground } from "./background.js";
+// import { SnowMountain } from "./snowMountain.js";
+
+
 
 export class PoolScene extends Scene {
 
-      name
+    name
 
-     constructor() {
+    constructor() {
         super()
-        
-         this.name = "pool"
+
+        this.name = "pool"
     }
 
 
     onActivate(ctx) {
         this.clear();
+
+        const poolbg = new PoolBackground({ pos: new Vector(0, 0) });
+        this.add(poolbg)
 
         let poolDoor = new PoolDoor();
         this.add(poolDoor)
@@ -30,6 +37,9 @@ export class PoolScene extends Scene {
         this.height = new Vector(30, 0)
 
         this.add(player)
+
+        // this.positionObstacle(SnowMountain, 8, this.obstaclePositions)
+
 
         const minX = 0;
         const maxX = 1240;
@@ -51,5 +61,52 @@ export class PoolScene extends Scene {
 
         let poolBorderBottom = new PoolBorderBottom();
         this.add(poolBorderBottom)
+    }
+
+    positionObstacle(ObstacleClass, number, positions) {
+        const obstacleCount = number
+        const width = 1240
+        const height = 920
+        // let minDistance = 100;
+        // let positions = [];
+        const obstacleSize = 100
+        const margin = obstacleSize / 2;
+        const minDistance = obstacleSize
+
+        function isFarEnough(x, y) {
+            for (let i = 0; i < positions.length; i++) {
+                let pos = positions[i];
+                let dx = pos.x - x;
+                let dy = pos.y - y;
+                if (Math.sqrt(dx * dx + dy * dy) < minDistance) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        let placed = 0;
+        while (placed < obstacleCount) {
+            let x = margin + Math.random() * (width - 2 * margin);
+            let y = margin + Math.random() * (height - 2 * margin);
+
+            // Check if any part of the obstacle would overlap the forbidden zone
+            if (
+                x + margin > 300 && x - margin < 700 &&
+                y + margin > 250 && y - margin < 600
+            ) {
+                continue;
+            }
+
+            if (isFarEnough(x, y)) {
+                let obstacle = new ObstacleClass();
+                obstacle.pos = new Vector(x, y);
+                this.add(obstacle);
+                console.log(x, y)
+                positions.push({ x: x, y: y });
+                placed++;
+            }
+        }
+
     }
 }
---
+++ src/js/poolgebied/snowMountain.js
@@ -0,0 +1,22 @@
+// import { Actor, Scene, Vector, CollisionType, Shape } from "excalibur";
+// import { Resources } from "../resources.js";
+
+// export class SnowMountain extends Actor {
+
+//     constructor() {
+//         super({ 
+//             width: Resources.Snowmountain.width, 
+//             height: Resources.Snowmountain.height, 
+//             collisionType: CollisionType.Fixed 
+//         })
+        
+//         this.graphics.use(Resources.SnowMountain.toSprite())
+//         this.pos = new Vector(500, 300)
+//         this.scale = new Vector(0.2, 0.2)
+//         this.collider.set(Shape.Box(300, 200, Vector.Zero, new Vector(-30, 0)));
+
+//     }
+
+
+
+// }
